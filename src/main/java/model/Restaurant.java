522c0013d030ef2ffca7c092de0ce3ba781a2b33
Nirvanas <nirvanas1994@yahoo.com>
2021-12-19 23:55:48 +0300

    [add] реализовал User и Restaurant.

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,11 +1,38 @@
 package model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Restaurant extends BaseModel {
+    private Set<Meal> menu = new HashSet<>();
+    private List<User> userList;
+    private String name;
+    private Integer countVotes;
+
+    public Restaurant(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public Restaurant(Integer id, String name, Collection<Meal> meals) {
+        super(id);
+        this.name = name;
+        this.menu.addAll(meals);
+    }
+
+    public Integer countingOfVotes() {
+        countVotes = getUserList().size();
+        return countVotes;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,14 +5,26 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
 @Getter
 @Setter
-@ToString(exclude = "name")
+@ToString
 @NoArgsConstructor
 public class User extends BaseModel {
+    private Set<Role> roles;
     private String name;
+    private List<Restaurant> restaurants;
 
-    public User(Integer id, String name) {
+    public User(Integer id, String name, Set<Role> roles) {
+        super(id);
         this.name = name;
+        setRole(roles);
+    }
+
+    public void setRole(Set<Role> roles) {
+        this.roles = EnumSet.copyOf(roles);
     }
 }
